691ba8d0faaeaea9d0f1414731213043d40b9f54
Gabriella Hevesy (CTW) <gabriella.hevesy@ctw.bmwgroup.com>
2024-05-12 23:42:46 +0100

    adds logic to buttons

--- app.js
+++ app.js
@@ -4,6 +4,35 @@ const toDoButton = document.querySelector('.todo-button');
 const toDoList = document.querySelector('.todo-list');
 
 // Event Listeners]
-
+toDoButton.addEventListener('click', addToDo);
 
 // Functions
+
+function addToDo(event) {
+    // Prevents form from submitting
+    event.preventDefault();
+
+    // Create To Do div
+    const toDoDiv = document.createElement('div');
+    toDoDiv.classList.add('todo');
+
+    // Create list item
+    const newToDo = document.createElement('li');
+    newToDo.innerText = 'hey';
+    newToDo.classList.add('todo-item');
+    toDoDiv.appendChild(newToDo);
+
+    // Check mark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fa-solid fa-check"></i>';
+    completedButton.classList.add('complete-btn');
+    toDoDiv.appendChild(completedButton);
+
+    // Rubbish button
+    const rubbishButton = document.createElement('button');
+    rubbishButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+    rubbishButton.classList.add('rubbish-btn');
+    toDoDiv.appendChild(rubbishButton);
+
+
+}
