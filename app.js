470071d30dfae984c7d7b391ed7f979c7025b94c
Gabriella Hevesy (CTW) <gabriella.hevesy@ctw.bmwgroup.com>
2024-05-12 22:06:16 +0100

    adds selectors

---
+++ app.js
@@ -0,0 +1,9 @@
+// Selectors
+const toDoInput = document.querySelector('.todo-input');
+const toDoButton = document.querySelector('.todo-button');
+const toDoList = document.querySelector('.todo-list');
+
+// Event Listeners]
+
+
+// Functions
